856de61f32b556c242191ea4cdbfab06a175de54
Satyajit Barik <117843736+satyajit7679@users.noreply.github.com>
2026-08-24 00:50:14 +0530

    ✅ NCOPIES - Rating 1745 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-23

--- codechef/medium/NCOPIES---Rating-1745/solution.py
+++ codechef/medium/NCOPIES---Rating-1745/solution.py
@@ -1,25 +1,17 @@
 # cook your dish here
-
 t = int(input())
-
 while t > 0:
-    n, m = map(int, input().split())
+    n, m = map(int,input().split())
     a = input()
-
-    total_ones = a.count('1') * m
-
-    prefix_ones = 0
+    total = a.count('1') * m
     ans = 0
-
-    for i in range(n * m):
-        if a[i % n] == '1':
-            prefix_ones += 1
-
-        suffix_ones = total_ones - prefix_ones
-
-        if prefix_ones == suffix_ones:
+    prefix = 0
+    for i in range(m * n):
+        curr = a[i % n]
+        if curr =='1':
+            prefix += 1
+        sufix = total - prefix
+        if prefix == sufix:
             ans += 1
-
-    print(ans)
-
     t -= 1
+    print(ans)
