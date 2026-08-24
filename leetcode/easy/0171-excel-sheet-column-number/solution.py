49c9e2656285e2670726ab0a97ef600dd27ee0a5
Satyajit Barik <117843736+satyajit7679@users.noreply.github.com>
2026-08-24 20:55:43 +0530

    ✅ Excel Sheet Column Number | Easy | Python | Runtime: 0 ms (100.00%) | Memory: 19.2 MB | 2026-08-24

--- leetcode/easy/0171-excel-sheet-column-number/solution.py
+++ leetcode/easy/0171-excel-sheet-column-number/solution.py
@@ -1,8 +1,7 @@
 class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
-        new_str = columnTitle.upper()
         res = 0
-        for s in new_str:
+        for s in columnTitle:
             res = res * 26 + (ord(s) - ord('A') + 1)
         return res
 
