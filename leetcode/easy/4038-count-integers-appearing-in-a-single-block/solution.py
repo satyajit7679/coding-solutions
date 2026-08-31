bc715a2dd44d5c8623d7789e45208774c054007e
Satyajit Barik <117843736+satyajit7679@users.noreply.github.com>
2026-08-31 23:40:52 +0530

    ✅ Count Integers Appearing in a Single Block | Easy | Python | Runtime: 0 ms | Memory: 19.4 MB | 2026-08-31

--- leetcode/easy/4038-count-integers-appearing-in-a-single-block/solution.py
+++ leetcode/easy/4038-count-integers-appearing-in-a-single-block/solution.py
@@ -1,7 +1,9 @@
 class Solution:
     def countSpecialIntegers(self, nums: list[int]) -> int:
-        i = 0
+        i = 1
         count = 0
+        if len(nums) == 1:
+            return 1
         while i < len(nums):
             if nums[i] == nums[i - 1]:
                 count += 1
