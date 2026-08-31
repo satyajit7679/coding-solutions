100c4a8e4fce3b46780e0555163b456b024a3e3d
Satyajit Barik <117843736+satyajit7679@users.noreply.github.com>
2026-09-01 01:07:27 +0530

    ✅ Ransom Note | Easy | Python | Runtime: 31 ms (12.17%) | Memory: 19.7 MB | 2026-08-31

---
+++ leetcode/easy/0383-ransom-note/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        have = {}
+        need = {}
+        for i in range(len(ransomNote)):
+            need[ransomNote[i]] = need.get(ransomNote[i], 0) + 1
+        
+        for i in range(len(magazine)):
+            have[magazine[i]] = have.get(magazine[i], 0) + 1
+        
+        for key,value in need.items():
+            if key not in have or value > have[key]:
+                return False
+        return True 
+
+
+        
+        
