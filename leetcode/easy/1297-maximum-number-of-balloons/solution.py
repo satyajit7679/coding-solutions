df9896a1f83e26e999eeccc294a7c6b4cabc951a
Satyajit Barik <117843736+satyajit7679@users.noreply.github.com>
2026-09-02 21:03:59 +0530

    ✅ Maximum Number of Balloons | Easy | Python | Runtime: 3 ms (62.56%) | Memory: 19.3 MB | 2026-09-02

---
+++ leetcode/easy/1297-maximum-number-of-balloons/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        s = "balloon"
+        need = {}
+        have = {}
+        res = float('inf')
+        for ch in s:
+            need[ch] = need.get(ch,0) + 1
+        for ch in text:
+            have[ch] = have.get(ch,0) + 1
+        for ch in need:
+            times = have.get(ch, 0) // need[ch]
+            res = min(res,times)  
+        return res
+        
