659df52cff185544ef8e5d6c31f0a7670a0f4a77
Satyajit Barik <117843736+satyajit7679@users.noreply.github.com>
2026-09-08 21:54:00 +0530

    ✅ Ceil in a Sorted Array | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-08

--- gfg/easy/ceil-in-a-sorted-array/solution.py
+++ gfg/easy/ceil-in-a-sorted-array/solution.py
@@ -1,38 +1,20 @@
-# class Solution:
-#     def findCeil(self, arr, x):
-#         # code here
-#         low = 0
-#         high = len(arr) - 1
-#         index = -1
-        
-#         while low <= high:
-#             mid = (low + high) // 2
-#             if arr[mid] == x:
-#                 return mid
-            
-#             if arr[mid] > x:
-#                 index = mid
-#                 high = mid -1
-                
-#             else:
-#                 low = mid + 1
-            
-#         return index
-
-
 class Solution:
     def findCeil(self, arr, x):
+        # code here
         low = 0
         high = len(arr) - 1
-        ans = -1
-
+        index = -1
+        
         while low <= high:
             mid = (low + high) // 2
-
+            
             if arr[mid] >= x:
-                ans = mid
-                high = mid - 1
+                index = mid
+                high = mid -1
+                
             else:
                 low = mid + 1
+            
+        return index
 
-        return ans
+
