41eda11fc221e71a2d41fd327dbac7e6391559cd
Satyajit Barik <117843736+satyajit7679@users.noreply.github.com>
2026-09-10 20:46:50 +0530

    ✅ Search in Rotated Sorted Array | Medium | Python | Runtime: 0 ms (100.00%) | Memory: 19.5 MB | 2026-09-10

--- leetcode/medium/0033-search-in-rotated-sorted-array/solution.py
+++ leetcode/medium/0033-search-in-rotated-sorted-array/solution.py
@@ -1,7 +1,8 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
         low = 0
-        high = len(nums) - 1
+        high = n - 1
 
         while low <= high:
             mid = (low + high) // 2
@@ -9,18 +10,32 @@ class Solution:
             if nums[mid] == target:
                 return mid
 
-            if nums[high] < nums[mid]:
-                # Left part is sorted
-                if nums[low] <= target < nums[mid]:
-                    high = mid - 1
-                else:
+            # find which part of my mid
+            #  if True then part 1
+            if nums[mid] > nums[n-1]: 
+                if nums[mid] < target:
                     low = mid + 1
-
+                else:
+                    if nums[0] > target:
+                        low = mid + 1
+                    else:
+                        high = mid - 1
+                continue
+            # part 2
+            
+            if nums[mid] > target:
+                high = mid - 1
             else:
-                # Right part is sorted
-                if nums[mid] < target <= nums[high]:
-                    low = mid + 1
-                else:
+                if nums[n-1] < target:
                     high = mid - 1
+                else:
+                    low = mid + 1
+        
+        return -1 
+
+
+
+
+
 
         return -1
