21196dfdef028a380445bed9e688b360bb8e1400
Satyajit Barik <117843736+satyajit7679@users.noreply.github.com>
2026-09-14 17:40:19 +0530

    ✅ Rotate String | Easy | Python | Runtime: 0 ms (100.00%) | Memory: 19.2 MB | 2026-09-14

--- leetcode/easy/0796-rotate-string/solution.py
+++ leetcode/easy/0796-rotate-string/solution.py
@@ -2,12 +2,9 @@ class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
         if len(s) != len(goal):
             return False
-        
-        if len(s) == 1:
-            return True
 
         if s == goal:
-            return False
+            return True
         
         return goal in (s + s)
 
