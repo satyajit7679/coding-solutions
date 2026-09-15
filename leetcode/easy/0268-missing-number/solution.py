e4464863497eb115df54a014b21f27d32f3f8a77
Satyajit Barik <117843736+satyajit7679@users.noreply.github.com>
2026-09-16 00:54:59 +0530

    ✅ Missing Number | Easy | Python | Runtime: 0 ms (100.00%) | Memory: 20.5 MB | 2026-09-15

--- leetcode/easy/0268-missing-number/solution.py
+++ leetcode/easy/0268-missing-number/solution.py
@@ -1,15 +1,8 @@
 class Solution:
     def missingNumber(self, nums: list[int]) -> int:
         n = len(nums)
-        freq = {}
-        for i in range(n + 1):
-            freq[i] = 0
-        
-        for num in nums:
-            freq[num] += 1
+        total = n * (n + 1) // 2
 
-        for i in range(n + 1):
-            if freq[i] == 0:
-                return i
+        return total - sum(nums)
 
         
