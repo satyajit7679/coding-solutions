d2bf1c5d8d7526d2ce45a445068701da1999d002
Satyajit Barik <117843736+satyajit7679@users.noreply.github.com>
2026-09-23 18:45:56 +0530

    ✅ Move Zeroes | Easy | Python | Runtime: 8 ms (22.68%) | Memory: 20.4 MB | 2026-09-23

--- leetcode/easy/0283-move-zeroes/solution.py
+++ leetcode/easy/0283-move-zeroes/solution.py
@@ -1,14 +1,15 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+        n = len(nums)
+        i = 0
         j = 0
-
-        for i in range(len(nums)):
+        while i < len(nums):
             if nums[i] != 0:
-                nums[i],nums[j] = nums[j],nums[i]
+                nums[j],nums[i] = nums[i],nums[j]
                 j += 1
-        
+            i += 1
+
+
         return nums
+
         
